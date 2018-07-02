1c4d874b432f8f9bdeea396f256f9361f5f01970
BruceZCQ <brucezcq@gmail.com>
2018-07-02 17:55:02 +0800

    add test

---
+++ src/test/java/cn/zhucongqi/demos/TestLauncher.java
@@ -0,0 +1,13 @@
+package cn.zhucongqi.demos;
+import org.junit.jupiter.api.Test;
+
+import cn.zhucongqi.server.Main;
+
+class TestLauncher {
+
+	@Test
+	void test() throws InterruptedException {
+		Main.main(new String[] {});
+	}
+
+}
