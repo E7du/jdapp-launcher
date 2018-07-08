b89ded2f29c414aa87680d82a00ba0abc3006b24
BruceZCQ <brucezcq@gmail.com>
2018-07-08 15:10:49 +0800

    update dubbo config

--- src/test/resources/jdapp-launcher-consumer.sh
+++ src/test/resources/jdapp-launcher-consumer.sh
@@ -4,5 +4,6 @@ JAVA_CMD="java"
 CPATH="./:lib/*:./:services/*"
 CFG_PATH="conf/"
 LOG4J_CFG="log4j.configuration=${CFG_PATH}log4j.properties"
+DUBBO_CFG="dubbo.properties.file=${CFG_PATH}dubbo.properties"
 
-${JAVA_CMD} -cp ${CPATH} -D${LOG4J_CFG} org.apache.dubbo.demo.consumer.Consumer 
+${JAVA_CMD} -cp ${CPATH} -D${LOG4J_CFG} -D${DUBBO_CFG} org.apache.dubbo.demo.consumer.Consumer
